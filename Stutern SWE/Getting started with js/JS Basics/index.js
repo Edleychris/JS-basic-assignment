120540074a2b0cfee9a7c955d38fac7e08f01448
Edleychris <edleychris.ec@gmail.com>
2023-02-12 14:54:48 +0100

    JS basics Assignment

---
+++ Stutern SWE/Getting started with js/JS Basics/index.js
@@ -0,0 +1,15 @@
+//A function that multiples two numbers.
+function mutiply(num1, num2) {
+    console.log (num1 * num2);
+}
+mutiply(4, 6);
+//Output 24;
+
+const firstName= "Nkiruka";
+const lastName = "Onwugbufor";
+//Different ways of declaring a variable
+let stuternTrack = "Front-End";
+var skills = "Technical Writing, GitHub, HTML, Bootstrap, JavaScript, and React.js. ";
+favColor = "Blue";
+yearOfCountryIndependence = 1960;
+console.log (`My name is ${firstName} ${lastName}. I am a ${stuternTrack} Developer proficient in ${skills}. My favourite color is ${favColor} and my country gained indepedence in ${yearOfCountryIndependence}. ` )
